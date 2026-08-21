2664394e18eec02eaaa0508649c039ef892ecef5
Christopher Galloway <christopher.galloway@wbscodingschool.com>
2026-08-21 15:07:15 +0200

    Add airport- and flights-data to database

--- 03_gans_schema.sql
+++ 03_gans_schema.sql
@@ -33,3 +33,29 @@ CREATE TABLE forecasts(
     forecast_id INT AUTO_INCREMENT PRIMARY KEY,
     FOREIGN KEY (city_id) REFERENCES cities(city_id)
 );
+
+CREATE TABLE airports(
+    icao CHAR(4) PRIMARY KEY,
+    `name` VARCHAR(80),
+    `active` BOOL DEFAULT 1,
+    city_id INT,
+    FOREIGN KEY (city_id) REFERENCES cities(city_id)
+);
+
+CREATE TABLE flights(
+	flight_id INT AUTO_INCREMENT PRIMARY KEY,
+	arrive_icao	CHAR(4),
+    depart_icao CHAR(4),	
+    depart_airport VARCHAR(80),	
+    depart_country CHAR(2),	
+    arrive_time_scheduled DATETIME,	
+    arrive_time_revised	DATETIME,
+    flight_number VARCHAR(32),	
+    aircraft VARCHAR(128),
+    FOREIGN KEY (arrive_icao) REFERENCES airports(icao)
+);
+
+# run after populating airports
+UPDATE airports
+SET `active` = 0 
+WHERE icao = 'EDDT';
